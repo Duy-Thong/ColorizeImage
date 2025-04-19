d1efb43cc23a4f970c15bac8c7a29ef942809736
SuZeAI <goohaa1234@gmail.com>
2025-04-19 14:28:06 +0700

    chore: Update .gitignore to exclude model directories and add specific model file exception

--- deepcolor/scripts/fetch_model.sh
+++ deepcolor/scripts/fetch_model.sh
@@ -1,1 +1,1 @@
-curl -O http://colorization.eecs.berkeley.edu/siggraph/models/caffemodel.pth & mv caffemodel.pth models/pytorch/
+curl -O models/pytorch/caffemodel.pth http://colorization.eecs.berkeley.edu/siggraph/models/caffemodel.pth 
